cdb14bd46d3dfd59be03077e2067bb09c8187cf7
Mrinal Choudhary <mrinal7331@gmail.com>
2024-09-10 07:59:06 +0530

    hard https://backend-v2-660423634636.us-central1.run.app/ for GCP

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   const dispatch = useDispatch();
 
   // const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
-  const BASE_URL = "http://35.224.132.64:5000/";
+  const BASE_URL = "https://backend-v2-660423634636.us-central1.run.app/";
 
   const getPortfolioData = async () => {
     try {
